90cfdab50d3a8b6fb861880d4893dec7698e5051
Kannan Annadurai <Kannaa.devops@gmail.com>
2024-01-25 21:34:00 +0400

    First committed

--- Jenkins-terraform/install_jenkins.sh
+++ Jenkins-terraform/install_jenkins.sh
@@ -1,16 +1,33 @@
 #!/bin/bash
-sudo apt update -y
-wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | tee /etc/apt/keyrings/adoptium.asc
-echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list
-sudo apt update -y
-sudo apt install temurin-17-jdk -y
-/usr/bin/java --version
-curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
-echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update -y
-sudo apt-get install jenkins -y
+#!/bin/bash
+sudo apt-get update
+yes | sudo apt install openjdk-11-jdk-headless
+echo "Waiting for 30 seconds before installing the jenkins package..."
+sleep 30
+sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
+  https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
+sudo apt-get update
+yes | sudo apt-get install jenkins
+sudo systemctl enable  jenkins
 sudo systemctl start jenkins
-sudo systemctl status jenkins
+
+
+##########Replaced my Own Jenkins Installation Scripts##############
+#sudo apt update -y
+#wget -O - https://packages.adoptium.net/artifactory/api/gpg/key/public | tee /etc/apt/keyrings/adoptium.asc
+#echo "deb [signed-by=/etc/apt/keyrings/adoptium.asc] https://packages.adoptium.net/artifactory/deb $(awk -F= '/^VERSION_CODENAME/{print$2}' /etc/os-release) main" | tee /etc/apt/sources.list.d/adoptium.list
+#sudo apt update -y
+#sudo apt install temurin-17-jdk -y
+#/usr/bin/java --version
+#curl -fsSL https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key | sudo tee /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+#echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] https://pkg.jenkins.io/debian-stable binary/ | sudo tee /etc/apt/sources.list.d/jenkins.list > /dev/null
+#sudo apt-get update -y
+#udo apt-get install jenkins -y
+##sudo systemctl start jenkins
+#sudo systemctl status jenkins
 
 #install docker
 sudo apt-get update
